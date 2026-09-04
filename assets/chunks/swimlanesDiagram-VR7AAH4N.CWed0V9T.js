2a9d9fbfbf4379791b61b6c875908e17f594b012
Yusei-YK <Yusei-YK@users.noreply.github.com>
2026-09-04 15:02:54 +0800

    docs: 环境脚本化（fetch-source / run-sim）

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.CWed0V9T.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.BrKhHYpH.js";import{_ as a}from"../app.D8oKaN2u.js";import"./chunk-5VM5RSS4.DSIx0sly.js";import"./chunk-XXDRQBXY.CZLMUufW.js";import"./chunk-POPQ4Y6H.CsbVJ04Q.js";import"./chunk-F27PBJKO.mAkfLOgl.js";import"./channel.De-T4cx7.js";import"./framework.9ngYh_O2.js";import"./theme.CsmwdEo1.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
